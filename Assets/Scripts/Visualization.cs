ecc857c35e537c42979ef78339bd63cfac8c1749
mdatsev <martin.datsev.p@gmail.com>
2020-10-24 17:57:06 +0300

    смалл фиь

--- Assets/Scripts/Visualization.cs
+++ Assets/Scripts/Visualization.cs
@@ -24,32 +24,28 @@ public class Visualization : MonoBehaviour
         edg.Add(new Edge(n1, n2, 1, 1, 60));
         //Node point1,point2;
 
-
         foreach (Edge e in edg)
         {
             int numofprefs = (int)e.length / 4;
-            Debug.Log(e.getStart().position.x + " patlak1 " + e.getStart().position.z);
-            Debug.Log(e.getEnd().position.x + " patlak2 " + e.getEnd().position.z);
+
             //Instantiate(street, new Vector3(e.getStart().position.x, 0, e.getStart().position.z), Quaternion.LookRotation(e.direction));
             for (int i=1; i <= numofprefs; i++)
             {
-               Instantiate(street, new Vector3(e.direction.x + 2*i, 0, e.direction.z + 2*i), Quaternion.LookRotation(e.direction));
+               Instantiate(street, new Vector3(e.direction.x + 2*i, 0, e.direction.z + 2*i), Quaternion.identity);
             }
             //Instantiate(street, new Vector3(e.getEnd().position.x, 0, e.getEnd().position.z), Quaternion.LookRotation(e.direction));
         }
 
         if (useDummySim) {
-            sim = new SimulationImpact();
-        } else {
             sim = new SimulationDummy();
+        } else {
+            sim = new SimulationImpact();
         }
-
         startingPoints.Add(new Vector2(0,0));
         startingPoints.Add(new Vector2(1,1));
 
         GameObject[] carListArray = Resources.LoadAll<GameObject>("Prefabs/Cars");
         List<GameObject> carList = carListArray.ToList();
-
         foreach (Vector2 p in startingPoints) {
             GameObject car = Instantiate(carList[Random.Range(0, carList.Count)], new Vector3(p.x, 0, p.y), Quaternion.identity);
             Car c = car.GetComponent<Car>();
