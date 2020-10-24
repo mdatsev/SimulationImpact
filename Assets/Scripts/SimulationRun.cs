1de89c1d3f15db5bc5dfd13fd42bf41f528eee39
mdatsev <martin.datsev.p@gmail.com>
2020-10-24 13:46:22 +0300

    sims polymorphism setup

--- Assets/Scripts/SimulationRun.cs
+++ Assets/Scripts/SimulationRun.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Simulations;
 
 public class SimulationRun : MonoBehaviour
 {
-    public GameObject simulation;
+    public bool useDummySim;
     public List<Vector2> startingPoints = new List<Vector2>();
+    private Simulation sim;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (useDummySim) {
+            sim = new SimulationImpact();
+        } else {
+            sim = new SimulationDummy();
+        }
+
         startingPoints.Add(new Vector2(0,0));
         startingPoints.Add(new Vector2(1,1));
     }
